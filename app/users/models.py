61b5ece3ff73eeb14066e47fdccaf9e5f6e633e5
RubioGarcia756 <ru1b10garc1a@gmail.com>
2024-06-21 12:15:42 -0600

    Cambios

--- app/users/models.py
+++ app/users/models.py
@@ -14,23 +14,13 @@ class kinship(enum.Enum):
     brother = "brother"
     sister = "sister"
 
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True, nullable=False)
-    kinship = Column(Enum(kinship))
-
-class university_degree(enum.Enum):
+class UniversityDegree(enum.Enum):
     DN = "DN"
     ASP = "ASP"
     QTA = "QTA"
     TICS = "TICS"
 
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True, nullable=False)
-    university_degree = Column(Enum(university_degree))
-
-class symptom(enum.Enum):
+class Symptom(enum.Enum):
     agresividad = "agresividad"
     alcoholismo = "alcoholismo"
     alucinaciones = "alucinaciones"
@@ -71,12 +61,7 @@ class symptom(enum.Enum):
     transtorno_auditivo = "transtorno_auditivo"
     capacidad_de_insigth = "capacidad_de_insigth"
 
-class user(base):
-    __tablename__ = "users"
-    id = Column (Integer, primary_key=True, index=True, autoincrement=True, nullable=False)
-    symptom = Column(Enum(symptom))
-
-class type_family(enum.Enum):
+class TypeFamily(enum.Enum):
     completa = "completa"
     incompleta = "incompleta"
     funcional = "funcional"
@@ -85,10 +70,7 @@ class type_family(enum.Enum):
     integrada = "integrada"
     desintegrada = "desintegrada"
     otro = "otro"
-class user(base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True, nullable=False)
-    type_family = Column(Enum(type_family))
+
         
 
 
@@ -101,19 +83,19 @@ class user(base):
     phone = Column(int, unique=True)
     religion = Column(String, nullable=False, unique=True)
     civil_status = Column(String, unique=True)
-    university_degree = Column(String, nullable=False)   #lleva clase
+    university_degree = Column(Enum(UniversityDegree))  #lleva clase
     registration = Column(String, unique=True)
     tutor = Column(String, unique=True)
     children = Column(bool, unique=True)
     assitance_psychologist = Column(String, unique=True)
     emergency_number = Column(int, unique=True)
-    kinchip = Column(String, nullable=False)             #lleva clase
+    kinship = Column(Enum(kinship))            #lleva clase
     note_one = Column(String, nullable=False)
-    symptom = Column(String, nullable=False)             #lleva clase
+    symptom = Column(Enum(Symptom))           #lleva clase
     type_psychologist = Column(String, nullable=False, unique=True)
     type_medic = Column(String, nullable=False)
     accident = Column(String, nullable=False)
-    type_family = Column(String, nullable=False)        #lleva clase
+    type_family = Column(Enum(TypeFamily))        #lleva clase
     diagnostic_print = Column(String, nullable=False)
     note_two = Column(String, nullable=False)
 
